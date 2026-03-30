d64d2dbdf133be7a226ba2198477a90674725d82
AndreisMarco <andreismarco2002@gmail.com>
2026-03-30 10:07:49 +0200

    adapted to new optimizer api and cleaned up boltz notebook

--- examples/boltz_notebook.py
+++ examples/boltz_notebook.py
@@ -1,6 +1,6 @@
 import marimo
 
-__generated_with = "0.15.5"
+__generated_with = "0.20.4"
 app = marimo.App(width="medium")
 
 with app.setup:
@@ -8,40 +8,23 @@ with app.setup:
     import marimo as mo
 
     import matplotlib.pyplot as plt
-    from mosaic.optimizers import simplex_APGM
+    from mosaic.optimizers import MultiPhaseOptimization, Phase, SimplexAPGM
     from mosaic.common import TOKENS
     import numpy as np
 
     from mosaic.notebook_utils import pdb_viewer
     import mosaic.losses.structure_prediction as sp
+    from mosaic.losses.protein_mpnn import FixedStructureInverseFoldingLL, InverseFoldingSequenceRecovery
 
     from mosaic.models.boltz2 import Boltz2
+    from mosaic.models.af2 import AlphaFold2
+    from mosaic.proteinmpnn.mpnn import ProteinMPNN
     from mosaic.structure_prediction import TargetChain
 
 
 @app.cell
 def _():
-    from mosaic.models.af2 import AlphaFold2
-    return (AlphaFold2,)
-
-
-@app.cell
-def _(AlphaFold2):
-    model_af = AlphaFold2()
-    return (model_af,)
-
-
-@app.cell
-def _():
-    model = Boltz2()
-    return (model,)
-
-
-@app.cell
-def _():
-    mo.md(
-        """
-    ---
+    mo.md("""
     **Warning**
 
     1. You'll almost certainly need a GPU or TPU to run this
@@ -49,50 +32,67 @@ def _():
     3. You might have to run these optimization methods multiple times before you get a reasonable binder
     4. If you change targets you'll likely have to fiddle with hyperparameters!
     5. This is pretty experimental, I highly recommend you stick with BindCraft if you're designing a minibinder against a protein target
-    ---
-    """
-    )
+    """)
+    return
+
+
+@app.cell(hide_code=True)
+def _():
+    mo.md(r"""
+    ## 1. Boltz2 binder optimization demo
+    """)
+    return
+
+
+@app.cell(hide_code=True)
+def _():
+    mo.md(r"""
+    We will use Boltz2 to optimize the binder sequence, while AF2 will serve for refolding as sanity check that our prediction are plausible.
+    """)
     return
 
 
 @app.cell
 def _():
+    model_af = AlphaFold2()
+    model = Boltz2()
+    return model, model_af
+
+
+@app.cell(hide_code=True)
+def _():
+    mo.md(r"""
+    In addition to the models above, we will be using ProteinMPNN as loss term to move the generated sequences towards sequences that AF2-multimer also likes. This is slower because we have to run the Boltz-2 structure module. Try removing it for faster generation!
+    """)
+    return
+
+
+@app.cell
+def _():
+    mpnn = ProteinMPNN.from_pretrained()
+    return (mpnn,)
+
+
+@app.cell(hide_code=True)
+def _():
+    mo.md(r"""
+    Define the target and dimension of the binder
+    """)
+    return
+
+
+@app.cell
+def _():
+    binder_length = 75
     target_sequence = "FTVTVPKDLYVVEYGSNMTIECKFPVEKQLDLAALIVYWEMEDKNIIQFVHGEEDLKVQHSSYRQRARLLKDQLSLGNAALQITDVKLQDAGVYRCMISYGGADYKRITVKVNA" 
-    return (target_sequence,)
+    return binder_length, target_sequence
 
 
-@app.cell
-def _(binder_length, model_af, target_sequence, template_st):
-    af_f, _ = model_af.binder_features(binder_length=binder_length, chains = [TargetChain(target_sequence, use_msa=False, template_chain=template_st.st[0][0])])
-    return (af_f,)
-
-
-@app.cell
-def _(af_pred):
-    af_pred.iptm
-    return
-
-
-@app.cell
-def _(PSSM_sharper, af_f, model_af):
-    af_pred = model_af.predict(features=af_f, PSSM = PSSM_sharper, writer = None, key = jax.random.key(12))
-    return (af_pred,)
-
-
-@app.cell
-def _(af_pred):
-    plt.imshow(af_pred.pae)
-    return
-
-
-@app.cell
+@app.cell(hide_code=True)
 def _():
-    return
-
-
-@app.cell
-def _(af_pred):
-    pdb_viewer(af_pred.st)
+    mo.md(r"""
+    Create Boltz2 features and structure writer and define the loss (note the inclusion of the inverse folding with ProteinMPNN)
+    """)
     return
 
 
@@ -103,19 +103,7 @@ def _(binder_length, model, target_sequence):
 
 
 @app.cell
-def _():
-    binder_length = 75
-    return (binder_length,)
-
-
-@app.cell
-def _(ProteinMPNN):
-    mpnn = ProteinMPNN.from_pretrained()
-    return (mpnn,)
-
-
-@app.cell
-def _(InverseFoldingSequenceRecovery, features, model, mpnn):
+def _(features, model, mpnn):
     loss = model.build_loss(
         loss=2 * sp.BinderTargetContact()
         + sp.WithinBinderContact()
@@ -125,62 +113,116 @@ def _(InverseFoldingSequenceRecovery, features, model, mpnn):
     return (loss,)
 
 
-@app.cell
+@app.cell(hide_code=True)
 def _():
-    mo.md("""Adding the ProteinMPNN term to the loss above tends to generate sequences that AF2-multimer also likes, but is slower because we have to run the Boltz-2 structure module. Try removing it for faster generation!""")
+    mo.md(r"""
+    Define the optimizer and run optimization on the binder
+    """)
     return
 
 
 @app.cell
-def _(binder_length, loss):
-    _, PSSM = simplex_APGM(
-        loss_function=loss,
-        n_steps=75,
-        x=jax.nn.softmax(
-            0.5*jax.random.gumbel(
-                key=jax.random.key(np.random.randint(100000)),
-                shape=(binder_length, 20),
+def _(loss):
+    optimizer = MultiPhaseOptimization(
+        phases=[
+            Phase(
+                name="soft",
+                return_best=True,
+                optimizer=SimplexAPGM(
+                    loss_fn=loss,
+                    # n_steps=75,
+                    n_steps=7,
+                    stepsize=0.1,
+                    scale=1.0,
+                    momentum=0.0,
+                )
+            ),
+            Phase(
+                name="sharp",
+                optimizer=SimplexAPGM(
+                    loss_fn=loss,
+                    # n_steps=50,
+                    n_steps=5,
+                    stepsize=0.5,
+                    scale=1.5, # increasing scale pushes the pssm towards onehot-like matrices
+                    momentum=0.0,
+                )
             )
-        ),
-        stepsize=0.1,
-        momentum=0.0,
+        ]
     )
-    return (PSSM,)
+    return (optimizer,)
 
 
 @app.cell
-def _(PSSM, loss):
-
-    PSSM_sharper, _ = simplex_APGM(
-        loss_function=loss,
-        n_steps=50,
-        x=PSSM,
-        stepsize = 0.5,
-        scale = 1.5,
-        momentum=0.0
+def _(binder_length, optimizer):
+    pssm_init = 0.5 * jax.random.gumbel(
+        key=jax.random.key(np.random.randint(100000)),
+        shape=(binder_length, 20),
     )
-    return (PSSM_sharper,)
+
+    pssm_sharp, intermediates, _  = optimizer.run(pssm_init) 
+    pssm_soft = intermediates["final"][0]
+    return pssm_sharp, pssm_soft
+
+
+@app.cell
+def _(pssm_sharp):
+    binder_seq = "".join(TOKENS[i] for i in pssm_sharp.argmax(-1))
+    binder_seq
+    return (binder_seq,)
+
+
+@app.cell
+def _(pssm_sharp):
+    plt.imshow(pssm_sharp)
+    return
+
+
+@app.cell(hide_code=True)
+def _():
+    mo.md(r"""
+    Repredict using Boltz2 both the soft and sharp version of the pssm
+    """)
+    return
 
 
 @app.cell
 def _(model):
     def predict(sequence, features, writer):
         return model.predict(PSSM = sequence, features = features, writer = writer, key = jax.random.key(0))
+
     return (predict,)
 
 
 @app.cell
-def _(PSSM, features, predict, structure_writer):
+def _(features, predict, pssm_soft, structure_writer):
     soft_pred = predict(
-        PSSM, features, structure_writer
+        pssm_soft, features, structure_writer
     )
     pdb_viewer(soft_pred.st)
     return (soft_pred,)
 
 
 @app.cell
-def _(soft_pred):
-    soft_pred.iptm
+def _(features, predict, pssm_sharp, structure_writer):
+    sharp_pred = predict(
+        pssm_sharp, features, structure_writer
+    )
+    pdb_viewer(sharp_pred.st)
+    return (sharp_pred,)
+
+
+@app.cell
+def _(sharp_pred, soft_pred):
+    print(f"{soft_pred.iptm=}")
+    print(f"{sharp_pred.iptm=}")
+    return
+
+
+@app.cell
+def _(sharp_pred, soft_pred):
+    plt.plot(sharp_pred.plddt)
+    plt.plot(soft_pred.plddt)
     return
 
 
@@ -194,46 +236,41 @@ def _(soft_pred):
 
 
 @app.cell
-def _(PSSM_sharper, features, predict, structure_writer):
-    pred = predict(
-        PSSM_sharper, features, structure_writer
-    )
-    pdb_viewer(pred.st)
-    return (pred,)
-
-
-@app.cell
-def _(pred):
-    mo.download(data=pred.st.make_pdb_string(), filename="a.pdb", label = "Boltz-2 predicted complex")
-    return
-
-
-@app.cell
-def _(pred):
+def _(sharp_pred):
     _f = plt.figure()
-    plt.imshow(pred.pae)
+    plt.imshow(sharp_pred.pae)
     plt.colorbar()
     _f
     return
 
 
-@app.cell
-def _(pred, soft_pred):
-    plt.plot(pred.plddt)
-    plt.plot(soft_pred.plddt)
+@app.cell(hide_code=True)
+def _():
+    mo.md(r"""
+    Save to disk the sharp structure
+    """)
     return
 
 
 @app.cell
-def _(PSSM_sharper):
-    binder_seq = "".join(TOKENS[i] for i in PSSM_sharper.argmax(-1))
-    binder_seq
+def _(sharp_pred):
+    mo.download(data=sharp_pred.st.make_pdb_string(), filename="a.pdb", label = "Boltz-2 predicted complex")
+    return
+
+
+@app.cell(hide_code=True)
+def _():
+    mo.md(r"""
+    ## 2. Refold with AF2
+    """)
     return
 
 
 @app.cell
 def _():
-    mo.md("""Make a template structure of the target alone we can use with AF2 multimer""")
+    mo.md("""
+    Make a template structure of the target alone we can use with AF2 multimer
+    """)
     return
 
 
@@ -255,65 +292,55 @@ def _(predict, target_sequence, template_features, template_writer):
 
 
 @app.cell
-def _(template_st):
-    template_st
+def _(binder_length, model_af, target_sequence, template_st):
+    af_features, _ = model_af.binder_features(binder_length=binder_length, chains = [TargetChain(target_sequence, use_msa=False, template_chain=template_st.st[0][0])])
+    return (af_features,)
+
+
+@app.cell(hide_code=True)
+def _():
+    mo.md(r"""
+    Then refold the optimized pssm in complex with the target
+    """)
     return
 
 
 @app.cell
-def _(PSSM_sharper):
-    plt.imshow(PSSM_sharper)
+def _(af_features, model_af, pssm_sharp):
+    af_pred = model_af.predict(features=af_features, PSSM = pssm_sharp, writer = None, key = jax.random.key(12))
+    pdb_viewer(af_pred.st)
+    return (af_pred,)
+
+
+@app.cell
+def _(af_pred):
+    print(f"{af_pred.iptm=}")
+    return
+
+
+@app.cell
+def _(af_pred):
+    plt.imshow(af_pred.pae)
     return
 
 
 @app.cell(hide_code=True)
-def _(PSSM):
-    plt.imshow(PSSM)
+def _():
+    mo.md(r"""
+    ## 3. Inverse fold of the predicted complex
+    """)
     return
 
 
-@app.cell
+@app.cell(hide_code=True)
 def _():
-    from mosaic.proteinmpnn.mpnn import ProteinMPNN
-    from mosaic.losses.protein_mpnn import FixedStructureInverseFoldingLL, InverseFoldingSequenceRecovery
-    return (
-        FixedStructureInverseFoldingLL,
-        InverseFoldingSequenceRecovery,
-        ProteinMPNN,
-    )
+    mo.md(r"""
+    Let's do it live! We can inverse fold the predicted complex using MPNN and the jacobi iteration in a few lines of code.
 
-
-@app.cell
-def _():
-    mo.md("""Let's do it live! We can inverse fold the predicted complex using MPNN and the jacobi iteration in a few lines of code.""")
+    At every iteration, to every position is assigned the amino acid that minimizes the loss gradients.
+    Note that optimization happens in discrete space (not in continuous space as for the other optimizers).
+    """)
     return
-
-
-@app.cell
-def _():
-    from mosaic.common import LossTerm
-    return (LossTerm,)
-
-
-@app.cell
-def _(LossTerm):
-    class GumbelPerturbation(LossTerm):
-        key: any
-
-        def __call__(self, sequence, key):
-            v = (jax.random.gumbel(self.key, sequence.shape)*sequence).sum()
-            return v, {"gumbel": v}
-    return (GumbelPerturbation,)
-
-
-@app.cell
-def _(FixedStructureInverseFoldingLL, mpnn, soft_pred):
-    if_ll = FixedStructureInverseFoldingLL.from_structure(
-            soft_pred.st,
-            mpnn,
-            stop_grad=True
-        )
-    return (if_ll,)
 
 
 @app.cell
@@ -327,57 +354,118 @@ def _():
             print(v)
 
         return sequence
+
     return (jacobi,)
 
 
 @app.cell
-def _(GumbelPerturbation, binder_length, if_ll, jacobi):
-    seq_mpnn = jacobi(
-        if_ll + 0.0005 * GumbelPerturbation(jax.random.key(np.random.randint(1000000))),
-        10,
-        np.random.randint(low=0, high=20, size=(binder_length)),
-        key=jax.random.key(np.random.randint(1000000)),
+def _(mpnn, soft_pred):
+    if_ll = FixedStructureInverseFoldingLL.from_structure(
+        st=soft_pred.st,
+        mpnn=mpnn,
+        name="if_ll",
+        stop_grad=True,
     )
+    return (if_ll,)
+
+
+@app.cell(hide_code=True)
+def _():
+    mo.md(r"""
+    Add random noise to gradients to avoid getting stuck in the same minima every time (increase diversity)
+    """)
     return
 
 
 @app.cell
 def _():
-    mo.md("""For fun let's design 10 complexes""")
+    from mosaic.common import LossTerm
+
+    return (LossTerm,)
+
+
+@app.cell
+def _(LossTerm):
+    class GumbelPerturbation(LossTerm):
+        key: any
+
+        def __call__(self, sequence, key):
+            v = (jax.random.gumbel(self.key, sequence.shape)*sequence).sum()
+            return v, {"gumbel": v}
+
+    return (GumbelPerturbation,)
+
+
+@app.cell
+def _(GumbelPerturbation, binder_length, if_ll, jacobi):
+    seq_mpnn = jacobi(
+        loss=if_ll + 0.0005 * GumbelPerturbation(jax.random.key(np.random.randint(1000000))),
+        iters=10,
+        sequence=np.random.randint(low=0, high=20, size=(binder_length)),
+        key=jax.random.key(np.random.randint(1000000)),
+    )
+    return (seq_mpnn,)
+
+
+@app.cell
+def _(binder_seq, seq_mpnn):
+    if_seq = "".join(TOKENS[i] for i in seq_mpnn)
+    print(f"Original optimized sequence: {binder_seq}")
+    print(f"Inverse folded sequence: {if_seq}")
+    return
+
+
+@app.cell(hide_code=True)
+def _():
+    mo.md(r"""
+    ## 4. Optimize batch of 10 binders
+    """)
+    return
+
+
+@app.cell
+def _():
+    mo.md("""
+    For fun let's design 10 complexes
+    """)
     return
 
 
 @app.cell
 def _(binder_length, features, loss, predict, structure_writer):
+    optimizer_batch = SimplexAPGM(
+        loss_fn=loss,
+        # n_steps=75,
+        n_steps=5,
+        stepsize=0.1,
+        momentum=0.9,
+    )
+
     def design():
-        _, PSSM = simplex_APGM(
-            loss_function=loss,
-            n_steps=75,
-            x=jax.nn.softmax(
-                0.5*jax.random.gumbel(
-                    key=jax.random.key(np.random.randint(100000)),
-                    shape=(binder_length, 20),
-                )
-            ),
-            stepsize=0.1,
-            momentum=0.9,
+        pssm_init = 0.5 * jax.random.gumbel(
+            key=jax.random.key(np.random.randint(100000)),
+            shape=(binder_length, 20),
         )
+        pssm, _, _ = optimizer_batch.run(pssm_init)
         prediction = predict(
-            PSSM, features, structure_writer
+            pssm, features, structure_writer
         )
         return prediction.st
+
     return (design,)
 
 
 @app.cell
 def _(design):
-    designs = [design() for _ in mo.status.progress_bar(range(10))]
+    # designs = [design() for _ in mo.status.progress_bar(range(10))]
+    designs = [design() for _ in mo.status.progress_bar(range(2))]
     return (designs,)
 
 
 @app.cell
 def _():
     from mosaic.notebook_utils import gemmi_structure_from_models
+
     return (gemmi_structure_from_models,)
 
 
